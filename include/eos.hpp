0d72d49e2f5d3b2778b39c1cd0b9c622759cb19a
Jacob Fields <jacob.fields@byu.edu>
2021-06-22 17:26:58 -0400

    Started working on EOS interface using policy-based design.

--- include/eos.hpp
+++ include/eos.hpp
@@ -1,13 +1,65 @@
 #ifndef EOS_HPP
 #define EOS_HPP
 
-/*!
- * \file eos.hpp
- * \brief defines EquationOfState, which should be compatible with the same Athena class.
- */
+//! \file eos.hpp
+//  \brief Defines an equation of state.
+//
+//  EOS is effectively an interface that describes how to create an
+//  equation of state. It must be instantiated with an object implementing
+//  the following functions:
+//    Real Temperature(Real n, Real e, Real *Y)
+//    Real Energy(Real n, Real T, Real *Y)
+//    Real Pressure(Real n, Real T, Real *Y)
+//    Real Entropy(Real n, Real T, Real *Y)
+//    Real Enthalpy(Real n, Real e, Real T)
+//  And it must also have the following member variables:
+//    const int NSpecies
 
-class EquationOfState{
+#include <ps_types.hpp>
+
+template <typename EOSPolicy>
+class EOS : private EOSPolicy {
+  private:
+    // Member functions
+    using EOSPolicy::Temperature;
+    using EOSPolicy::Energy;
+    using EOSPolicy::Pressure;
+    using EOSPolicy::Entropy;
+    using EOSPolicy::Enthalpy;
+
+    // Member variables
+    // The number of particle species used by the EOS.
+    using EOSPolicy::NSpecies;
+    
   public:
-};
+    //! \fn Real GetTemperature(Real n, Real e, Real *Y)
+    //  \brief Calculate the temperature from number density, energy density, and
+    //         particle fractions.
+    //
+    //  \param[in] n  The number density
+    //  \param[in] e  The energy density
+    //  \param[in] Y  An array of particle fractions, expected to be of size NSpecies.
+    //  \returns The temperature according to the EOS.
+    inline Real GetTemperature(Real n, Real e, Real *Y){
+      return Temperature(n, p, Y);
+    }
+
+    //! \fn Real GetEnergy(Real n, Real T, Real *Y)
+    //  \brief Get the energy density from the numbe density, temperature, and
+    //         particle fractions.
+    //
+    //  \param[in] n  The number density
+    //  \param[in] T  The temperature
+    //  \param[in] Y  An array of size NSpecies of the particle fractions.
+    //  \returns The energy density according to the EOS.
+
+
+
+    //! \fn int GetNSpecies() const
+    //  \brief Get the number of particle species in this EOS.
+    inline int GetNSpecies() const {
+      return NSpecies;
+    }
+}
 
 #endif
--- include/ps_types.hpp
+++ include/ps_types.hpp
@@ -1,13 +1,19 @@
 #ifndef PS_TYPES_HPP
 #define PS_TYPES_HPP
 
-/*!
- * \file ps_types.hpp
- * \brief contains some basic type definitions consistent with Athena++.
- */
+//! \file ps_types.hpp
+//  \brief contains some basic type definitions consistent with Athena++.
+// 
+//  Ideally this file shouldn't be required when the code is dropped into Athena.
+//  Therefore, all type definitions should be consistent with Athena.
+//
 
-namespace PrimitiveSolver{
-  using Real = double;
-};
+#define NHYDRO 5
+
+using Real = double;
+enum ConsIndex {IDN=0, IM1=1, IM2=2, IM3=3, IEN=4};
+enum PrimIndex {IVX=1, IVY=2, IVZ=3, IPR=4, IBY=(NHYDRO), IBZ=((NHYDRO)+1)};
+enum MagneticIndex {IB1=0, IB2=1, IB3=2};
+enum SpatialMetricIndex{S11=0, S12=1, S13=2, S22=3, S23=4, S33=5, NSPMETRIC=6};
 
 #endif
