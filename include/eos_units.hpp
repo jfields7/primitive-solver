d4700a95a70b84d192434481da8c60d033ff7ae9
Jacob Fields <jacob.fields@byu.edu>
2021-07-09 15:05:38 -0400

    Finished refactoring EOSUnits. Someone should double-check the units to make sure that they're consistent.

--- include/eos_units.hpp
+++ include/eos_units.hpp
@@ -5,85 +5,62 @@
 //  \brief contains unit definitions and conversion for the EOS solver.
 //  
 //  Each unit system is defined as its own struct inside the EOSUnits namespace.
+//  TODO: Check that these conversions are correct.
 
 namespace EOSUnits{
 
 //! CGS Units
 struct CGS {
-  static Real const c   = 2.99792458e10; //! cm/s
-  static Real const G   = 6.67430e-8; //! cm^3 g^-1 s^-2
-  static Real const kb  = 1.380649e-16; //! erg K^-1
-  static Real const MeV = 1.602176634e-6; //! erg
+  static Real const c    = 2.99792458e10; //! cm/s
+  static Real const G    = 6.67430e-8; //! cm^3 g^-1 s^-2
+  static Real const kb   = 1.380649e-16; //! erg K^-1
+  static Real const Msun = 1.98847e33; //! g
+  static Real const MeV  = 1.602176634e-6; //! erg
 
-  static Real const length  = 1.0; //! cm
-  static Real const density = 1.0; //! cm^-3
+  static Real const length  = 1.0;
+  static Real const density = 1.0;
+  static Real const mass = 1.0;
+  static Real const energy = 1.0;
+  static Real const pressure = 1.0;
+  static Real const temperature = 1.0;
 };
 
 //! Geometric units
 struct Geometric {
-  static Real const c   = 1.0;
-  static Real const G   = 1.0;
-  static Real const kb  = 1.0;
-  static Real const MeV = 1.32383331356638e-55; //! cm
+  static Real const c    = 1.0;
+  static Real const G    = 1.0;
+  static Real const kb   = 1.0;
+  static Real const Msun = 1.4766696910334395;  //! km
+  static Real const MeV  = 1.32383331356638e-60; //! km
 
-  static Real const length = 1.0; //! cm/cm
-  static Real const density = 1.0; //! cm^3/cm^3
+  // Geometric units for converting from their cgs equivalent.
+  static Real const length      = 1.0e-5; //! km/cm
+  static Real const density     = 1.0e15; //! cm^3/km^3
+  static Real const mass        = 7.426160269118665e-34; //! erg*G/c^2*km/cm
+  static Real const energy      = 8.26271763969804e-55; //! cm*c^4/G/erg
+  static Real const pressure    = 8.26271763969804e-40; //! c^4/G/cm^2/erg
+  static Real const temperature = 1.140791284653146e-70; //! cm*c^4/G*kb/erg
 };
 
 //! Nuclear units
+//
+//  TODO: Especially double-check these.
 struct Nuclear {
-  static Real const c;
-  static Real const kb  = 1.0;
-  static Real const MeV = 1.0;
+  static Real const c    = 1.0;
+  static Real const G    = 1.323833313566383e-42; //! fm
+  static Real const kb   = 1.0;
+  static Real const Msun = 1.115449865100704e60; //! MeV
+  static Real const MeV  = 1.0;
 
-  static Real const length  = 1e13; //! fm/cm
-  static Real const density = 1e-39; //! cm^3/fm^3
+  // Nuclear units for converting from their cgs equivalent.
+  static Real const length      = 1e13; //! fm/cm
+  static Real const density     = 1e-39; //! cm^3/fm^3
+  static Real const mass        = 5.609588603804452e26; //! MeV/(erg/c^2)
+  static Real const energy      = 6.241509074460763e5;  //! MeV/erg
+  static Real const pressure    = 6.241509074460763e34; //! (MeV/fm^3)/(erg/cm^3)
+  static Real const temperature = 8.61733326214518e-11; //! MeV/(erg/kb)
 };
 
-  // Speed of light
-  const double c_in_cgs = 2.99792458e10; // cgs units -- cm/s
-  const double c_in_si  = 2.99792458e8;  // SI units -- m/s
-  const double c_in_nat = 1.0; // Natural units
-  const double c_in_code = 1.0; // Code units
-
-  // Gravitational constant
-  const double G_in_cgs = 6.67430e-8;  // cgs units -- cm^3 g^-1 s^-2
-  const double G_in_si  = 6.67430e-11; // SI units -- m^3 kg^-1 s^-2
-  const double G_in_nat = 1.0; // Natural units
-  const double G_in_code = 1.0; // Code units
-
-  // Boltzmann constant
-  const double kb_in_cgs = 1.380649e-16; // cgs units -- erg K^-1
-  const double kb_in_si  = 1.380649e-23; // SI units -- J K^-1
-  const double kb_in_mev = 8.617333262145e1; // MeV/K
-  const double kb_in_nat = 1.0; // natural units
-  const double kb_in_code = 1.0; // Code units
-
-
-  //Units
-
-  // Length unit (fm)
-  const double length_in_cgs = 1e-13; // cgs units -- cm
-  const double length_in_si  = 1e-15; // SI units -- m
-  const double length_in_nuc = 1.0;   // nuclear units
-  const double length_in_code = 1.0; // Code units
-  
-  // Number density unit (fm^-3)
-  const double nb_in_cgs = 1e39; // cgs units -- cm^-3
-  const double nb_in_si  = 1e45; // SI units -- m^-3
-  const double nb_in_nuc = 1.0;  // nuclear units
-  const double nb_in_code = 1.0; // Code units
-
-  // Energy units (MeV)
-  const double energy_in_cgs  = 1.602176634e-6;  // cgs units -- erg
-  const double energy_in_si   = 1.602176634e-13; // SI units -- J
-  const double energy_in_natm = 1.323833314e-57; // Natural units -- m
-  const double energy_in_nuc  = 1.0; // Nuclear units
-  const double energy_in_code = 1.0; // Code units
-
-  // Temperature (MeV)
-
-  // Chemical potential (MeV)
-};
+}; // namespace
 
 #endif
